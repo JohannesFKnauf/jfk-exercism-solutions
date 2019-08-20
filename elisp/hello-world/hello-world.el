858b2b5c4c7b6ff772e1b94310d0b6fd5cc9eaba
Johannes F. Knauf <johannes.f.knauf@metamorphant.de>
2019-08-20 21:42:49 +0200

    add elisp hello-world

---
+++ elisp/hello-world/hello-world.el
@@ -0,0 +1,13 @@
+;;; hello-world.el --- Hello World Exercise (exercism)
+
+;;; Commentary:
+
+;;; Code:
+(defun hello ()
+  "Create a friendly greeting."
+  "Hello, World!")
+
+
+(provide 'hello-world)
+;;; hello-world.el ends here
+
