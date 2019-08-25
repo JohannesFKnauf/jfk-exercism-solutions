6dfbe0b72d82501334cb5567acc57ddf990de389
Johannes F. Knauf <johannes.f.knauf@metamorphant.de>
2019-08-25 16:30:21 +0200

    add clojure clock refactored

--- clojure/clock/src/clock.clj
+++ clojure/clock/src/clock.clj
@@ -1,14 +1,29 @@
 (ns clock)
 
+(defn clock->hours [clock]
+  (quot clock 60))
+
+(defn clock->minutes [clock]
+  (mod clock 60))
+
 (defn clock->string [clock]
   (format "%02d:%02d"
-          (quot clock 60)
-          (mod clock 60)))
+          (clock->hours clock)
+          (clock->minutes clock)))
+
+(def minutes-per-day
+  (* 24 60))
+
+(defn hours->minutes [hours]
+  (* 60 hours))
 
 (defn clock [hours minutes]
-  (mod (+ (* 60 hours) minutes)
-       (* 24 60)))
+  (-> hours
+      (hours->minutes)
+      (+ minutes)
+      (mod minutes-per-day)))
 
 (defn add-time [clock time]
-  (mod (+ clock time)
-       (* 24 60)))
+  (-> clock
+      (+ time)
+      (mod minutes-per-day)))
