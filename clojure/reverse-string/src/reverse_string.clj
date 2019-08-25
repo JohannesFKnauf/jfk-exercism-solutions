b08107a95f528049bcf6e739b6d5331655b5313d
Johannes F. Knauf <johannes.f.knauf@metamorphant.de>
2019-08-25 17:08:30 +0200

    recursive experiment with clojure reverse-string

--- clojure/reverse-string/src/reverse_string.clj
+++ clojure/reverse-string/src/reverse_string.clj
@@ -1,4 +1,8 @@
 (ns reverse-string)
 
 (defn reverse-string [s]
-  (reduce #(str %2 %1) "" s))
+  (loop [result ""
+         remainder s]
+    (if (empty? remainder) result
+        (recur (str (first remainder) result)
+               (rest remainder)))))
