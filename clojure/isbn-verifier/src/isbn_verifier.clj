529052ceca0e000e4f3c33558f73f6bd7e3ecc52
Johannes F. Knauf <johannes.f.knauf@metamorphant.de>
2019-08-28 17:59:08 +0200

    refactor clojure isbn verifier: sanitize input before reading digits/letters

--- clojure/isbn-verifier/src/isbn_verifier.clj
+++ clojure/isbn-verifier/src/isbn_verifier.clj
@@ -2,9 +2,9 @@
 
 (defn isbn? [isbn]
   (boolean (some->> isbn
-                    (re-matches #"(\d)-?(\d)(\d)(\d)-?(\d)(\d)(\d)(\d)(\d)-?([\dX])")
-                    (rest)
-                    (map #(if (= % "X") 10 (Integer/parseInt %)))
+                    (#(clojure.string/replace % "-" ""))
+                    (re-matches #"\d{9}[\dX]")
+                    (map #(if (= % \X) 10 (Integer/parseInt (str %))))
                     (map * (range 10 0 -1))
                     (apply +)
                     (#(mod % 11))
