208431afe8509d8d4f050404350c1fe907df601e
Johannes F. Knauf <johannes.f.knauf@metamorphant.de>
2019-08-28 18:19:50 +0200

    refactor clojure sublist for better comprehensibility

--- clojure/sublist/src/sublist.clj
+++ clojure/sublist/src/sublist.clj
@@ -1,16 +1,13 @@
 (ns sublist)
 
 (defn sublist? [a b]
-  (cond
-    (< (count b) (count a)) false
-    (not-any? false? (map = a b)) true
-    :else (recur a (drop 1 b))))
+  (->> b
+       (partition (count a) 1)
+       (some #(= a %))))
 
 (defn classify [a b]
-  (let [a-sub-b (sublist? a b)
-        b-sub-a (sublist? b a)]
-    (cond
-      (and a-sub-b b-sub-a) :equal
-      a-sub-b :sublist
-      b-sub-a :superlist
-      :else :unequal)))
+  (cond
+    (= a b) :equal
+    (sublist? a b) :sublist
+    (sublist? b a) :superlist
+    :else :unequal))
