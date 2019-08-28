d4513b6027d660dd037828740bfcdc77481b92cd
Johannes F. Knauf <johannes.f.knauf@metamorphant.de>
2019-08-28 18:16:50 +0200

    clojure etl: refactor to for comprehension

--- clojure/etl/src/etl.clj
+++ clojure/etl/src/etl.clj
@@ -1,10 +1,7 @@
 (ns etl)
 
-(defn key-list-to-flat-map [key list]
-  (reduce #(assoc %1 (clojure.string/lower-case %2) key) {} list))
-
-(defn key-list-into-map [result key list]
-  (merge result (key-list-to-flat-map key list)))
-
 (defn transform [source]
-  (reduce-kv key-list-into-map {} source))
+  (into {}
+        (for [[score letters] source
+              letter letters]
+          [(clojure.string/lower-case letter) score])))
