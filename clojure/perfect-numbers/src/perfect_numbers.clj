c386d454f94fdb7cf659feb07a26355f05670e96
Johannes F. Knauf <johannes.f.knauf@metamorphant.de>
2019-08-29 18:11:38 +0200

    refactor clojure perfect numbers: extract logic from classify

--- clojure/perfect-numbers/src/perfect_numbers.clj
+++ clojure/perfect-numbers/src/perfect_numbers.clj
@@ -4,13 +4,15 @@
   (zero? (mod num div)))
 
 (defn factors [number]
-  (let [max-candidate (quot number 2)
-        candidates (range 1 (inc max-candidate))]
-    (filter (partial divisible-by? number) candidates)))
+  (if (pos? number) 
+    (let [max-candidate (quot number 2)
+          candidates (range 1 (inc max-candidate))]
+      (filter (partial divisible-by? number) candidates))
+    (throw (IllegalArgumentException. "Can not classify negative numbers."))))
 
 (defn classify [number]
   (let [aliquot-sum (apply + (factors number))]
-    (cond (< number 0) (throw (IllegalArgumentException. "Can not classify negative numbers."))
-          (= number aliquot-sum) :perfect
-          (< number aliquot-sum) :abundant
-          (> number aliquot-sum) :deficient)))
+    (condp apply [aliquot-sum number]
+      > :abundant
+      < :deficient
+      = :perfect)))
