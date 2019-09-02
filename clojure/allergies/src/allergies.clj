efab88915c760a311fe64abaa58f4e33d22db528
Johannes F. Knauf <johannes.f.knauf@metamorphant.de>
2019-09-02 06:55:34 +0200

    refactor clojure allergies by chosing more specific higher-order-functions

--- clojure/allergies/src/allergies.clj
+++ clojure/allergies/src/allergies.clj
@@ -4,20 +4,10 @@
   [:eggs :peanuts :shellfish :strawberries :tomatoes :chocolate :pollen :cats])
 
 (def allergene-map
-  (zipmap (iterate (partial * 2) 1) allergenes))
-
-(defn test-allergy [code [bit allergy]]
-  (->> bit
-       (bit-and code)
-       (not= 0)))
-
-(defn allergies [code]
-  (->> allergene-map
-       (filter (partial test-allergy code))
-       (map second)))
+  (zipmap allergenes (range)))
 
 (defn allergic-to? [code allergene]
-  (-> code
-      (allergies)
-      (set)
-      (contains? allergene)))
+  (bit-test code (get allergene-map allergene)))
+
+(defn allergies [code]
+  (filter (partial allergic-to? code) allergenes))
