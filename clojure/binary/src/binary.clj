83efc6e77158c418b19f4412d11940587e7407cb
Johannes F. Knauf <johannes.f.knauf@metamorphant.de>
2019-09-02 06:48:34 +0200

    clojure binary: extract explicit precheck

--- clojure/binary/src/binary.clj
+++ clojure/binary/src/binary.clj
@@ -1,11 +1,13 @@
 (ns binary)
 
+(def powers-of-2
+  (iterate #(* 2 %) 1))
+
 (defn to-decimal [radix-2-num]
-  (->> radix-2-num
-       (map #(= \1 %))
-       (reverse)
-       (map list
-            (iterate (partial * 2) 1))
-       (filter second)
-       (map first)
-       (reduce +)))
+  (let [parsed-bits (map {\1 1 \0 0} radix-2-num)]
+    (if (some nil? parsed-bits)
+      0
+      (->> parsed-bits
+           (reverse)
+           (map * powers-of-2)
+           (reduce +)))))
