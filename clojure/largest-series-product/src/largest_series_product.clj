89854e6d51107fb0f6b205fa93478f5ac2237af8
Johannes F. Knauf <johannes.f.knauf@metamorphant.de>
2019-09-02 22:14:04 +0200

    clojure largest series product: inline products, but prevalidate

--- clojure/largest-series-product/src/largest_series_product.clj
+++ clojure/largest-series-product/src/largest_series_product.clj
@@ -4,11 +4,11 @@
   {:pre [(>= n 0)
          (every? #(Character/isDigit %) s)
          (>= (count s) n)]}
-  (let [products (->> s
-                      (map str)
-                      (map #(Integer/parseInt %))
-                      (partition n 1)
-                      (map (partial reduce *)))]
-    (if (seq products)
-      (apply max products)
-      1)))
+  (if (empty? s)
+    1
+    (->> s
+         (map str)
+         (map #(Integer/parseInt %))
+         (partition n 1)
+         (map (partial reduce *))
+         (apply max))))
