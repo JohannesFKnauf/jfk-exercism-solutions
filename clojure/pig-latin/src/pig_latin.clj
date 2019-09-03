b136ce926e539dee7786c3d80425703787709f1d
Johannes F. Knauf <johannes.f.knauf@metamorphant.de>
2019-09-03 04:55:25 +0200

    clojure pig latin: improve variable naming

--- clojure/pig-latin/src/pig_latin.clj
+++ clojure/pig-latin/src/pig_latin.clj
@@ -1,11 +1,11 @@
 (ns pig-latin
   (:require clojure.string))
 
-(def starts-with-vowel #"^((?:[aeiou]|xr|yt)\w*)()$")
-(def starts-with-consonant #"^([bcdfghjklmnpqrstvwxz]*qu|[bcdfghjklmnpqrstvwxyz][bcdfghjklmnpqrstvwxz]*)(\w*)$")
+(def vowel-rule #"^((?:[aeiou]|xr|yt)\w*)()$")
+(def consonant-rule #"^([bcdfghjklmnpqrstvwxz]*qu|[bcdfghjklmnpqrstvwxyz][bcdfghjklmnpqrstvwxz]*)(\w*)$")
 
 (defn translate-word [s]
-  (->> [starts-with-vowel starts-with-consonant]
+  (->> [vowel-rule consonant-rule]
        (some #(re-matches %1 s))
        (#(str (% 2) (% 1) "ay"))))
 
