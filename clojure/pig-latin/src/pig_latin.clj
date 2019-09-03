bc700bce8942b921756a81ac7097e061cc2cc3cf
Johannes F. Knauf <johannes.f.knauf@metamorphant.de>
2019-09-03 04:52:33 +0200

    clojure pig latin: add complete regex solution

--- clojure/pig-latin/src/pig_latin.clj
+++ clojure/pig-latin/src/pig_latin.clj
@@ -1,26 +1,12 @@
 (ns pig-latin
-  (:use [clojure.string :only [replace split join]]))
-
-
-(def starts-with-vowel #"^([aeiou]|xr|yt)\w*")
-
-(def starts-with-consonant #"^([bcdfghjklmnpqrstvwxyz]+)(\w*)")
-
-(def starts-with-consonant-and-qu #"^([bcdfghjklmnpqrstvwxz]*qu)(\w*)")
-
-(def starts-with-consonant-and-y #"^([bcdfghjklmnpqrstvwxz]+y)(\w*)")
-
-
-
-(defn translate-word [s]
-  (condp re-matches s
-    starts-with-vowel (str s "ay")
-    starts-with-consonant-and-qu (replace s starts-with-consonant-and-qu "$2$1ay")
-    starts-with-consonant-and-y (replace s starts-with-consonant-and-y "$2$1ay")
-    starts-with-consonant (replace s starts-with-consonant "$2$1ay")
-    s))
+  (:require clojure.string))
 
 (defn translate [s]
-  (join " "
-        (map translate-word
-             (split s #" "))))
+  (clojure.string/replace s (re-pattern
+                             (str "\\b("
+                                  (clojure.string/join "|" ["(?:[aeiou]|xr|yt)\\w*"
+                                                            "[bcdfghjklmnpqrstvwxz]*qu"
+                                                            "[bcdfghjklmnpqrstvwxz]+y"
+                                                            "[bcdfghjklmnpqrstvwxyz]+"])
+                                  ")(\\w*)\\b"))
+                          "$2$1ay"))
