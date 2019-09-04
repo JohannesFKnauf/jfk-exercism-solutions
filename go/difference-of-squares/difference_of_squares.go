76b1fbeac5f6a49323828278f32f26d026feffac
Johannes F. Knauf <johannes.f.knauf@metamorphant.de>
2019-09-04 05:50:35 +0200

    go differences of squares: leverage math

--- go/difference-of-squares/difference_of_squares.go
+++ go/difference-of-squares/difference_of_squares.go
@@ -4,21 +4,14 @@ package diffsquares
 // SquareOfSum calculates the square of the sum of all integers from 1..n
 // example: SquareOfSum(4) = (1+2+3+4)**2
 func SquareOfSum(n int) int {
-	var x int
-	for i := 1; i <= n; i++ {
-		x += i
-	}
-	return x * x
+	sum := n * (n + 1) / 2
+	return sum * sum
 }
 
 // SumOfSquares calculates the sum of the squares of all integers from 1..n
 // example: SumOfSquares(4) = 1**2 + 2**2 + 3**2 + 4**2
 func SumOfSquares(n int) int {
-	var x int
-	for i := 1; i <= n; i++ {
-		x += i * i
-	}
-	return x
+	return (n * (n + 1) * (2*n + 1)) / 6
 }
 
 // Difference calculates the difference between square-of-sums and sum-of-squares for all integers 1..n
