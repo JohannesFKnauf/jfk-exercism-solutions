12d7f9ec210c1b7a8b4e0fdb8ed0b9414877cfbc
Johannes F. Knauf <johannes.f.knauf@metamorphant.de>
2019-09-04 17:26:17 +0200

    inline helper functions

--- haskell/anagram/src/Anagram.hs
+++ haskell/anagram/src/Anagram.hs
@@ -1,24 +1,18 @@
 module Anagram (anagramsFor) where
 
+import Control.Monad (liftM2)
+import Data.Function (on)
 import Data.List (filter, sort)
 import Data.Char (toLower)
 
-normalizeCase :: String -> String
-normalizeCase =
-  map toLower
-
-normalize :: String -> String
-normalize =
-  sort . normalizeCase
-
-isAnagram :: String -> String -> Bool
-isAnagram ref cand =
-  (normalize ref) == (normalize cand)
-
-isSameWord :: String -> String -> Bool
-isSameWord ref cand =
-  (normalizeCase ref) == (normalizeCase cand)
-
 anagramsFor :: String -> [String] -> [String]
-anagramsFor ref xs =
-  filter (isAnagram ref) (filter (not . (isSameWord ref)) xs)
+anagramsFor ref =
+  let
+    normalizeCase = map toLower
+    normalize = sort . normalizeCase
+    equivalentAfter = on (==)
+    hasSameLettersAs = equivalentAfter normalize
+    isSameWordAs = equivalentAfter normalizeCase
+    (.&&.) = liftM2 (&&)
+  in
+    filter ((not . isSameWordAs ref) .&&. (hasSameLettersAs ref))
