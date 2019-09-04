0c38e3fd331b9677250572e8fca67aacd98ab007
Johannes F. Knauf <johannes.f.knauf@metamorphant.de>
2019-09-04 06:07:57 +0200

    prolog queen attack: combine predicates for attack

--- prolog/queen-attack/queen_attack.pl
+++ prolog/queen-attack/queen_attack.pl
@@ -1,6 +1,7 @@
 create((X, Y)) :- between(0, 7, X), between(0, 7, Y).
 
-attack((_, B), (_, Y)) :- Y = B.
-attack((A, _), (X, _)) :- A = X.
-attack((A, B), (X, Y)) :- Y - B =:= X - A.
-attack((A, B), (X, Y)) :- A - X =:= Y - B.
+attack((A, B), (X, Y)) :- create((A, B)), 
+                          create((X, Y)), 
+                          (Y = B;
+                           A = X;
+                           abs(Y - B) =:= abs(X - A)).
