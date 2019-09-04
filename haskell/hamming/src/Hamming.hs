986b3cebc79819ab6ab86284a63a74388ad2190f
Johannes F. Knauf <johannes.f.knauf@metamorphant.de>
2019-09-04 17:30:33 +0200

    haskell hamming: refactor to use helper function zipWithOrFail

--- haskell/hamming/src/Hamming.hs
+++ haskell/hamming/src/Hamming.hs
@@ -1,6 +1,13 @@
 module Hamming (distance) where
 
+import Control.Monad (liftM)
+
 distance :: String -> String -> Maybe Int
-distance xs ys
-  | length xs == length ys = Just . length . filter not $ zipWith (==) xs ys
-  | otherwise = Nothing
+distance xs ys =
+  liftM (length . filter not) . sequence $ zipWithOrFail (==) xs ys
+
+zipWithOrFail :: (a -> a -> b) -> [a] -> [a] -> [Maybe b]
+zipWithOrFail _ [] [] = []
+zipWithOrFail _ _ [] = [Nothing]
+zipWithOrFail _ [] _ = [Nothing]
+zipWithOrFail f (x:xs) (y:ys) = Just (f x y) : (zipWithOrFail f xs ys)
