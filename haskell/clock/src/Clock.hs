935c719f0289a0436f9b12370aae2912a139ea42
Johannes F. Knauf <johannes.f.knauf@metamorphant.de>
2019-09-04 16:47:33 +0200

    haskell clock: change from type to data

--- haskell/clock/src/Clock.hs
+++ haskell/clock/src/Clock.hs
@@ -2,20 +2,21 @@ module Clock (addDelta, fromHourMin, toString) where
 
 import Text.Printf (printf)
 
-type Clock = Int
+-- Clock is just a container for minutes
+data Clock = Clock Int deriving (Eq, Show)
 
 
 fromHourMin :: Int -> Int -> Clock
-fromHourMin h m = totalMin `mod` minPerDay
+fromHourMin h m = Clock $ totalMin `mod` minPerDay
   where
     minPerDay = 24 * 60
     totalMin  = (h * 60) + m
 
 
 toString :: Clock -> String
-toString clock = printf "%02d:%02d" h m
+toString (Clock totalMin) = printf "%02d:%02d" h m
   where
-    (h, m) = clock `divMod` 60
+    (h, m) = totalMin `divMod` 60
 
 addDelta :: Int -> Int -> Clock -> Clock
-addDelta h m clock = fromHourMin h (m + clock)
+addDelta h m (Clock totalMin) = fromHourMin h (m + totalMin)
