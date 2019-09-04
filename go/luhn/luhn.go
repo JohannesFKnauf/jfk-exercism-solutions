c0b507e92feb5ddbb962dbdb4e01c4764117b0ce
Johannes F. Knauf <johannes.f.knauf@metamorphant.de>
2019-09-04 05:47:19 +0200

    go luhn: iterate backwards explicitly

--- go/luhn/luhn.go
+++ go/luhn/luhn.go
@@ -6,37 +6,30 @@ import (
 	"unicode"
 )
 
-// Reverse reverts a string
-func Reverse(s string) string {
-	runes := []rune(s)
-	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
-		runes[i], runes[j] = runes[j], runes[i]
-	}
-	return string(runes)
-}
-
 // Valid checks for luhn validity of a given input number
 func Valid(input string) bool {
 	var alt bool // alt is false for each 'first' digit and true for each 'second' digit
-	var n int    // n keeps the number of valid digits seen so far
+	var digits int
 	var sum int
-	for _, r := range Reverse(input) {
+	rs := []rune(input)
+	for i := len(rs) - 1; i >= 0; i-- {
+		r := rs[i]
 		if unicode.IsDigit(r) {
-			n++
-			i, _ := strconv.Atoi(string(r))
+			digits++
+			n, _ := strconv.Atoi(string(r))
 			if alt {
-				x := i * 2
+				x := n * 2
 				if x > 9 {
 					x -= 9
 				}
 				sum += x
 			} else {
-				sum += i
+				sum += n
 			}
 			alt = !alt
 		} else if !unicode.IsSpace(r) {
 			return false
 		}
 	}
-	return n > 1 && sum%10 == 0
+	return digits > 1 && sum%10 == 0
 }
