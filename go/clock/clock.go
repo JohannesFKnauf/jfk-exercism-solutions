3bc5f8d05f59732f54135a8826bcb1e805d6688c
Johannes F. Knauf <johannes.f.knauf@metamorphant.de>
2019-09-04 05:45:09 +0200

    go clock: extract mod logic to New

--- go/clock/clock.go
+++ go/clock/clock.go
@@ -5,39 +5,37 @@ import (
 	"fmt"
 )
 
-// Clock represents the state of a clock as minutes
-type Clock int
-
-// Mod calculates (always positive) modulus of a number n for a divisor m
-func Mod(n, m int) int {
-	x := n % m
-	if (x < 0 && m > 0) || (x > 0 && m < 0) {
-		return x + m
-	}
-	return x
+// Clock represents the state of a clock as a positive amount of minutes
+type Clock struct {
+	mins int
 }
 
 // New creates a clock from a given hour h and minute m
+// Neither h nor m need to be positive numbers
 func New(h, m int) Clock {
-	ms := Mod(m+60*h, 1440)
-	return Clock(ms)
+	mins := m + 60*h
+	mins = mins % (24 * 60)
+	if mins < 0 {
+		mins += (24 * 60)
+	}
+	return Clock{mins}
 }
 
 // String converts a clock into its human-readable 24h clock representation
 func (c Clock) String() string {
-	m := int(c) % 60
-	h := (int(c) - m) / 60
+	m := c.mins % 60
+	h := (c.mins - m) / 60
 	return fmt.Sprintf("%02d:%02d", h, m)
 }
 
 // Add adds minutes to a given clock
 func (c Clock) Add(m int) Clock {
-	mp := Mod(int(c)+m, 1440)
-	return Clock(mp)
+	mins := c.mins + m
+	return New(0, mins)
 }
 
 // Subtract subtracts minutes from a given clock
 func (c Clock) Subtract(m int) Clock {
-	mp := Mod(int(c)-m, 1440)
-	return Clock(mp)
+	mins := c.mins - m
+	return New(0, mins)
 }
