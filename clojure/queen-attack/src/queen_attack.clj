b2dfd31017569a74195df512658ca1da19b3cf8c
Johannes F. Knauf <johannes.f.knauf@metamorphant.de>
2019-09-08 21:09:15 +0200

    add clojure queen attack first try

---
+++ clojure/queen-attack/src/queen_attack.clj
@@ -0,0 +1,36 @@
+(ns queen-attack)
+
+(defn abs [x]
+  (max x (- x)))
+
+(def empty-board
+  (->> "_"
+       (repeat 8)
+       vec
+       (repeat 8)
+       vec))
+
+(defn place-queen [board positions key color]
+  (if-let [position (get positions key)] 
+    (assoc-in board position color)
+    board))
+
+(defn render-board [board]
+  (->> board
+       (map (partial clojure.string/join " "))
+       (map #(str %1 "\n"))
+       clojure.string/join))
+
+(defn board-string [positions]
+  (-> empty-board
+      (place-queen positions :w "W")
+      (place-queen positions :b "B")
+      render-board))
+
+(defn can-attack [{[xb yb] :b [xw yw] :w}]
+  (cond
+    (= xb xw) true
+    (= yb yw) true
+    (= (abs (- yw yb))
+       (abs (- xb xw))) true
+    :else false))
