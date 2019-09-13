6c2b712ac1410d430bc572b66166fa338981902c
Johannes F. Knauf <johannes.f.knauf@metamorphant.de>
2019-09-13 08:22:41 +0200

    refactor clojure prime factors: pass numbers, not seqs

--- clojure/prime-factors/src/prime_factors.clj
+++ clojure/prime-factors/src/prime_factors.clj
@@ -6,24 +6,16 @@
 (defn max-true-prime-factor [n]
   (Math/floor (Math/sqrt n)))
 
-(defn prime-factor-candidates
-  ([n]
-   (prime-factor-candidates n 2))
-  ([n start]
-   (range start (inc (max-true-prime-factor n)))))
-
 (defn of
   ([n]
-   (of n (prime-factor-candidates n)))
-  ([n candidates]
+   (of n 2 (max-true-prime-factor n)))
+  ([n candidate max-prime]
    (cond
      (= 1 n) (list)
-     (nil? candidates) (list n)
-     (empty? candidates) (list n)
-     (divisible-by? n (first candidates)) (let [d (first candidates)
-                                                r (quot n d)]
-                                            (lazy-seq (cons d
-                                                            (of r
-                                                                (prime-factor-candidates r d)))))
-     :else (lazy-seq (of n (next candidates))))))
+     (< max-prime candidate) (list n)
+     (divisible-by? n candidate) (let [r (quot n candidate)]
+                                   (lazy-seq (cons candidate
+                                                   (of r candidate (max-true-prime-factor r)))))
+     :else (lazy-seq
+            (of n (inc candidate) max-prime)))))
 
