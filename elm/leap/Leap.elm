51c64736cb0239c1f26abfbdec63e6a0ba858beb
Johannes F. Knauf <johannes.f.knauf@metamorphant.de>
2019-10-17 19:38:42 +0200

    add elm leap

---
+++ elm/leap/Leap.elm
@@ -0,0 +1,11 @@
+module Leap exposing (..)
+
+isLeapYear : Int -> Bool
+isLeapYear year =
+    if divisibleBy 400 year then True
+    else if divisibleBy 100 year then False
+    else if divisibleBy 4 year then True
+    else False
+
+divisibleBy a b =
+    remainderBy a b == 0
