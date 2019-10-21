f284ff84f71cd9b3b940f805d20f095572d2f4a5
Johannes F. Knauf <johannes.f.knauf@metamorphant.de>
2019-10-21 20:50:10 +0200

    elm space age: extract constants, extract redundant operations

--- elm/space-age/SpaceAge.elm
+++ elm/space-age/SpaceAge.elm
@@ -10,22 +10,24 @@ type Planet
   | Uranus
   | Neptune
 
-planetYearInSeconds : Planet -> Float
-planetYearInSeconds planet =
-    let
-        earthYearSeconds = 31557600.0
-    in
-        case planet of
-            Earth -> 1.0 * earthYearSeconds
-            Mercury -> 0.2408467 * earthYearSeconds
-            Venus -> 0.61519726  * earthYearSeconds
-            Mars -> 1.8808158 * earthYearSeconds
-            Jupiter -> 11.862615 * earthYearSeconds
-            Saturn -> 29.447498 * earthYearSeconds
-            Uranus -> 84.016846 * earthYearSeconds
-            Neptune -> 164.79132 * earthYearSeconds
+earthPeriodSeconds = 31557600.0
 
+periodEarthYears : Planet -> Float
+periodEarthYears planet =
+    case planet of
+        Earth -> 1.0
+        Mercury -> 0.2408467
+        Venus -> 0.61519726
+        Mars -> 1.8808158
+        Jupiter -> 11.862615
+        Saturn -> 29.447498
+        Uranus -> 84.016846
+        Neptune -> 164.79132
+
+periodSeconds : Planet -> Float
+periodSeconds =
+    periodEarthYears >> (*) earthPeriodSeconds
 
 ageOn : Planet -> Float -> Float
 ageOn planet ageSeconds =
-    ageSeconds / (planetYearInSeconds planet)
+    ageSeconds / (periodSeconds planet)
