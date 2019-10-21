812f809adeb9f6e31417c1e1b04330e7cd972f93
Johannes F. Knauf <johannes.f.knauf@metamorphant.de>
2019-10-21 17:51:56 +0200

    unify earth formula in elm space age

--- elm/space-age/SpaceAge.elm
+++ elm/space-age/SpaceAge.elm
@@ -10,19 +10,22 @@ type Planet
   | Uranus
   | Neptune
 
-planetYearLength : Planet -> Float
-planetYearLength planet =
-    case planet of
-        Earth -> 31557600.0
-        Mercury -> 0.2408467 * planetYearLength Earth
-        Venus -> 0.61519726  * planetYearLength Earth
-        Mars -> 1.8808158 * planetYearLength Earth
-        Jupiter -> 11.862615 * planetYearLength Earth
-        Saturn -> 29.447498 * planetYearLength Earth
-        Uranus -> 84.016846 * planetYearLength Earth
-        Neptune -> 164.79132 * planetYearLength Earth
+planetYearInSeconds : Planet -> Float
+planetYearInSeconds planet =
+    let
+        earthYearSeconds = 31557600.0
+    in
+        case planet of
+            Earth -> 1.0 * earthYearSeconds
+            Mercury -> 0.2408467 * earthYearSeconds
+            Venus -> 0.61519726  * earthYearSeconds
+            Mars -> 1.8808158 * earthYearSeconds
+            Jupiter -> 11.862615 * earthYearSeconds
+            Saturn -> 29.447498 * earthYearSeconds
+            Uranus -> 84.016846 * earthYearSeconds
+            Neptune -> 164.79132 * earthYearSeconds
 
 
 ageOn : Planet -> Float -> Float
 ageOn planet ageSeconds =
-    ageSeconds / (planetYearLength planet)
+    ageSeconds / (planetYearInSeconds planet)
