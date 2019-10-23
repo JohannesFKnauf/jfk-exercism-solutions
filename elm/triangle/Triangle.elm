17402c0abd32658d6ff12c0975e3ad974100e6cb
Johannes F. Knauf <johannes.f.knauf@metamorphant.de>
2019-10-23 16:51:26 +0200

    refactor elm triangle

--- elm/triangle/Triangle.elm
+++ elm/triangle/Triangle.elm
@@ -1,30 +1,23 @@
 module Triangle exposing (..)
-
-
-version : Int
-version =
-    2
         
 type Triangle = Equilateral
-    | Isosceles
-    | Scalene
+              | Isosceles
+              | Scalene
         
 triangleKind : number -> number -> number -> Result String Triangle
 triangleKind a b c =
-    if hasVanishingEdge a b c then (Err "Invalid lengths")
-    else if isViolatingEquality a b c then (Err "Violates inequality")
-    else if isEquilateral a b c then (Ok Equilateral)
-    else if isIsosceles a b c then (Ok Isosceles)
-    else (Ok Scalene)
-
-hasVanishingEdge a b c =
-    a <= 0 || b <= 0 || c <= 0
-
-isViolatingEquality a b c =
-    c > a + b || b > a + c || a > b + c
-
-isEquilateral a b c =
-    a == b && b == c
-
-isIsosceles a b c =
-    a == b || b == c || a == c
+    let
+        hasVanishingEdge =
+            a <= 0 || b <= 0 || c <= 0
+        isViolatingEquality =
+            c > a + b || b > a + c || a > b + c
+        isEquilateral =
+            a == b && b == c
+        isIsosceles =
+            a == b || b == c || a == c
+    in
+        if hasVanishingEdge then (Err "Invalid lengths")
+        else if isViolatingEquality then (Err "Violates inequality")
+        else if isEquilateral then (Ok Equilateral)
+        else if isIsosceles then (Ok Isosceles)
+        else (Ok Scalene)
