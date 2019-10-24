ee86e672993e9214a4e1d3ebc8daa1227f6d6ca0
Johannes F. Knauf <johannes.f.knauf@metamorphant.de>
2019-10-24 20:16:17 +0200

    refactor go parallel letter frequency: avoid sync

--- go/parallel-letter-frequency/parallel_letter_frequency.go
+++ go/parallel-letter-frequency/parallel_letter_frequency.go
@@ -1,32 +1,24 @@
 // Package letter provides a parallel letter count algorithm
 package letter
 
-import (
-	"sync"
-)
-
 const numWorkers = 4
 
-func Distribute(ss []string) chan string {
-	tasks := make(chan string)
-	go func() {
-		for _, s := range ss {
-			tasks <- s
-		}
-		close(tasks)
-	}()
-	return tasks
+func distribute(ss []string, tasks chan<- string) {
+	for _, s := range ss {
+		tasks <- s
+	}
 }
 
-func Reducer(tasks <-chan string, result chan<- FreqMap) {
+func reduce(tasks <-chan string, result chan<- FreqMap) {
 	for task := range tasks {
 		result <- Frequency(task)
 	}
 }
 
-func Combine(subResults <-chan FreqMap) FreqMap {
+func combine(subResults <-chan FreqMap, numResults int) FreqMap {
 	r := FreqMap{}
-	for f := range subResults {
+	for i := 0; i < numResults; i++ {
+		f := <-subResults
 		for k, v := range f {
 			r[k] += v
 		}
@@ -36,30 +28,25 @@ func Combine(subResults <-chan FreqMap) FreqMap {
 
 // ConcurrentFrequency counts letters in text using a map-reduce like approach
 // ss is an array of substrings to be counted
-// individual substrings are counted concurrently (shared-nothing) in 4 go routines
+// individual substrings are counted concurrently (shared-nothing) in numWorkers (default: 4) go routines
 // the final result is combined sequentially
 func ConcurrentFrequency(ss []string) FreqMap {
-	tasks := Distribute(ss)
+	taskCount := len(ss)
 
+	tasks := make(chan string)
 	subResults := make(chan FreqMap)
-	var wg sync.WaitGroup
-
-	for i := 0; i < numWorkers; i++ {
-		wg.Add(1)
-		go func() {
-			Reducer(tasks, subResults)
-			wg.Done()
-		}()
-	}
 
 	go func() {
-		wg.Wait()
-		close(subResults)
+		distribute(ss, tasks)
+		close(tasks)
 	}()
-	
 
-	r := Combine(subResults)
+	for i := 0; i < numWorkers; i++ {
+		go reduce(tasks, subResults)
+	}
+
+	r := combine(subResults, taskCount)
+	close(subResults)
 
 	return r
 }
-
