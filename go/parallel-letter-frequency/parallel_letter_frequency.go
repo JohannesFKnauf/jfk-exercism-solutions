f91373baa8b2ae4fcddd5e741d8f484c146be324
Johannes F. Knauf <johannes.f.knauf@metamorphant.de>
2019-10-28 16:52:57 +0100

    refactor go parallel letter frequency: reduce complexity

--- go/parallel-letter-frequency/parallel_letter_frequency.go
+++ go/parallel-letter-frequency/parallel_letter_frequency.go
@@ -1,42 +1,27 @@
 // Package letter provides a parallel letter count algorithm
 package letter
 
-const numWorkers = 4
-
 // ConcurrentFrequency counts letters in text using a map-reduce like approach
 // ss is an array of substrings to be counted
 // individual substrings are counted concurrently (shared-nothing) in numWorkers (default: 4) go routines
 // the final result is combined sequentially
 func ConcurrentFrequency(ss []string) FreqMap {
-	taskCount := len(ss)
+	freqs := make(chan FreqMap)
 
-	tasks := make(chan string)
-	subResults := make(chan FreqMap)
-
-	go func() {
-		for _, s := range ss {
-			tasks <- s
-		}
-		close(tasks)
-	}()
-
-	for i := 0; i < numWorkers; i++ {
-		go func() {
-			for task := range tasks {
-				subResults <- Frequency(task)
-			}
-		}()
+	for _, s := range ss {
+		go func(s string) {
+			freqs <- Frequency(s)
+		}(s)
 	}
 
 	r := FreqMap{}
+	taskCount := len(ss)
 	for i := 0; i < taskCount; i++ {
-		f := <-subResults
+		f := <-freqs
 		for k, v := range f {
 			r[k] += v
 		}
 	}
-
-	close(subResults)
 
 	return r
 }
