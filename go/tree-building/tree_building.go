2bcf280e77abe65cbfd186888f22ec50b8bfb0a3
Johannes F. Knauf <johannes.f.knauf@metamorphant.de>
2019-11-17 20:05:36 +0100

    refactor go tree building: use switch-case for checking preconditions, improve naming, use (faster) array instead of map

--- go/tree-building/tree_building.go
+++ go/tree-building/tree_building.go
@@ -21,39 +21,38 @@ type Node struct {
 // Build assembles a tree from a set of records
 // it returns a pointer to the root node
 func Build(records []Record) (*Node, error) {
-	lookup := make(map[int]*Node)
+	n := len(records)
+	if n == 0 {
+		return nil, nil
+	}
+	nodes := make([]*Node, n)
+
 	sort.Slice(records, func(i, j int) bool { return records[i].ID < records[j].ID })
 
 	for i, record := range records {
-		if _, ok := lookup[record.ID]; ok {
-			return nil, fmt.Errorf("Record with ID %d already existed. This should not happen with a list of unique records.", record.ID)
-		}
-		if i != record.ID {
+		switch {
+		case i > record.ID:
+			return nil, fmt.Errorf("Records contain duplicates. Expected next record.ID '%d', but got '%d'.", i, record.ID)
+		case i < record.ID:
 			return nil, fmt.Errorf("Records are non-continuous. Expected next record.ID '%d', but got '%d'.", i, record.ID)
-		}
-		lookup[record.ID] = &Node{
-			ID: record.ID,
-		}
-	}
-	for _, record := range records {
-		parent := lookup[record.Parent]
-		if parent == nil {
-			return nil, fmt.Errorf("Parent '%d' does not exist.", record.Parent)
-		}
-
-		if record.ID == 0 && record.Parent != 0 {
+		case record.ID == 0 && record.Parent != 0:
 			return nil, fmt.Errorf("Root node '%v' does not reference itself.", record)
-		}
-		if record.ID != 0 && record.Parent == record.ID {
+		case record.ID != 0 && record.Parent == record.ID:
 			return nil, fmt.Errorf("Non-root node '%v' is referencing itself.", record)
-		}
-		if record.ID != 0 && record.Parent > record.ID {
+		case record.ID != 0 && record.Parent > record.ID:
 			return nil, fmt.Errorf("Parent ID '%d' higher than record ID '%d'.", record.Parent, record.ID)
 		}
-		node := lookup[record.ID]
-		if node != parent {
-			parent.Children = append(parent.Children, lookup[record.ID])
+
+		node := Node{
+			ID: record.ID,
 		}
+		nodes[i] = &node
+
+		if record.ID != record.Parent {
+			parent := nodes[record.Parent]
+			parent.Children = append(parent.Children, &node)
+		}
+
 	}
-	return lookup[0], nil
+	return nodes[0], nil
 }
