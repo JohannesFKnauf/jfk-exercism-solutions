b9a4716552ae750877d9605f0eef0cf028428bad
Johannes F. Knauf <johannes.f.knauf@metamorphant.de>
2019-12-27 15:54:57 +0100

    experiment with boolean operators in elisp leap

--- elisp/leap/leap.el
+++ elisp/leap/leap.el
@@ -8,10 +8,9 @@
 
 (defun leap-year-p (year)
   "Check if a given year is a leap year."
-  (cond ((divisible-by-p 400 year) t)
-	((divisible-by-p 100 year) nil)
-	((divisible-by-p 4 year) t)
-	(t nil)))
+  (or (divisible-by-p 400 year)
+      (and (not (divisible-by-p 100 year))
+	   (divisible-by-p 4 year))))
 
 (provide 'leap)
 ;;; leap.el ends here
