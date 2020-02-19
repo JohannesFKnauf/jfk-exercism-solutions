400b9a2e7cf206fbcacb2f521a4fe9b9f3454f55
Johannes F. Knauf <johannes.f.knauf@metamorphant.de>
2020-02-19 07:14:01 +0100

    add first try guile RNA transcription

---
+++ scheme/rna-transcription/rna-transcription.scm
@@ -0,0 +1,12 @@
+(import (rnrs))
+
+(define (transcribe nucleotide)
+  (case nucleotide
+   ((#\G) #\C)
+   ((#\C) #\G)
+   ((#\T) #\A)
+   ((#\A) #\U)))
+
+(define (dna->rna dna)
+  (string-map transcribe dna))
+
