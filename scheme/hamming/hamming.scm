319575c29c770ba870ba11c887d56fae04739938
Johannes F. Knauf <johannes.f.knauf@metamorphant.de>
2020-04-15 21:13:25 +0200

    scheme hamming: use pure fold

--- scheme/hamming/hamming.scm
+++ scheme/hamming/hamming.scm
@@ -1,15 +1,12 @@
 (import (rnrs))
 
-(define (different? nucl-a nucl-b)
-  (not (equal? nucl-a
-               nucl-b)))
-
 (define (hamming-distance strand-a strand-b)
   (unless (= (string-length strand-a)
              (string-length strand-b))
     (raise "strands have different length"))
-   (apply + 
-    (map (lambda (a b) (if (char=? a b) 0 1))
-         (string->list strand-a)
-         (string->list strand-b))))
-
+  (fold-left (lambda (cur a b)
+               (+ cur
+                  (if (char=? a b) 0 1)))
+             0
+             (string->list strand-a)
+             (string->list strand-b)))
