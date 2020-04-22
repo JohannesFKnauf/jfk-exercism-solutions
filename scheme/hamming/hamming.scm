3b6074e0d31bd8011666ae969ebb5c3d910661c9
Johannes F. Knauf <johannes.f.knauf@metamorphant.de>
2020-04-22 20:47:37 +0200

    scheme hamming: extract diff-score

--- scheme/hamming/hamming.scm
+++ scheme/hamming/hamming.scm
@@ -1,12 +1,17 @@
 (import (rnrs))
 
+(define (diff-score a b)
+  (if (char=? a b)
+      0
+      1))
+
 (define (hamming-distance strand-a strand-b)
   (unless (= (string-length strand-a)
              (string-length strand-b))
     (raise "strands have different length"))
   (fold-left (lambda (cur a b)
                (+ cur
-                  (if (char=? a b) 0 1)))
+                  (diff-score a b)))
              0
              (string->list strand-a)
              (string->list strand-b)))
